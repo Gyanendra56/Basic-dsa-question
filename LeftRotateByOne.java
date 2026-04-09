e5da29ac8e9997a4466ff97fe29fcc170551af85
Gyanendra Gangwar <160461469+Gyanendra56@users.noreply.github.com>
2026-04-09 20:50:05 +0530

    Add files via upload

---
+++ LeftRotateByOne.java
@@ -0,0 +1,20 @@
+public class LeftRotateByOne {
+    public static void rotate(int[] arr) {
+        int first = arr[0];
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            arr[i] = arr[i + 1];
+        }
+
+        arr[arr.length - 1] = first;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        rotate(arr);
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
